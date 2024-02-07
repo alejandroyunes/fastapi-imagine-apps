0f976a6af4bc419dfe009dd64edbbb39620248c4
Alejandro Yunes <alejandroyunes@outlook.com>
2024-02-07 03:59:47 -0500

    added PUT method

--- main.py
+++ main.py
@@ -73,3 +73,11 @@ def delete_task(task_id: int):
     global tasks
     tasks = [task for task in tasks if task["id"] != task_id]
     return {"message": "Task deleted successfully"}
+
+@app.put("/tasks/{task_id}")
+def update_task(task_id: int, updated_task: Task):
+    for task in tasks:
+        if task["id"] == task_id:
+            task["reminder"] = not task["reminder"]
+            return task 
+    raise HTTPException(status_code=404, detail="Task not found")
